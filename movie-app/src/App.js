c2a6b1c68b28372cde786a853e55451c59d88eac
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-04 10:52:53 +0000

    Exercise 5 Completed. Navigation Bar and Routing

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
