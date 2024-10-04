4f1294a5fe53c05281a6fa8b1992fa2d912a1c5b
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-10-04 10:59:29 +0000

    Exercise 6 Completed. Client Side Routing

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -11,9 +11,9 @@ function App() {
     <Router>
       <NavigationBar />
       <Routes>
-        <Route path="/home" element={<Content />} />
-        <Route path="/read" element={<h1>Read Component</h1>} />
-        <Route path="/create" element={<h1>Create Component</h1>} />
+        <Route path="/home" element={<Content/>} />
+        <Route path="/read" element={<Footer/>} />
+        <Route path="/create" element={<Header/>} />
       </Routes>
       <Footer />
     </Router>
